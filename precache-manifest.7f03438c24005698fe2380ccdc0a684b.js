fe1528b58bfccff8f677a82f7fc403c80d1bad82
franklin-jr <franklin_juniorn@hotmail.com>
2021-01-28 17:58:26 -0300

    gh-pages

---
+++ precache-manifest.7f03438c24005698fe2380ccdc0a684b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e8953b235cf6ed7090",
+    "url": "/vuejs-gh-pages-gh-actions/css/app.de8b2514.css"
+  },
+  {
+    "revision": "885746b51c5405f33af3",
+    "url": "/vuejs-gh-pages-gh-actions/css/chunk-7d480729.24d418b7.css"
+  },
+  {
+    "revision": "869a596717edc6bbabfe",
+    "url": "/vuejs-gh-pages-gh-actions/css/chunk-vendors.0c1df0af.css"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/vuejs-gh-pages-gh-actions/fonts/pokemon_solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "54e1c73dfe6eedcd3ba28ea086cdaa53",
+    "url": "/vuejs-gh-pages-gh-actions/fonts/trebuc.54e1c73d.ttf"
+  },
+  {
+    "revision": "14540979a6685ab378bf0cee1e97f942",
+    "url": "/vuejs-gh-pages-gh-actions/img/bug.14540979.svg"
+  },
+  {
+    "revision": "8f2a7aaa9ae7bd49980f1b2b99a2577f",
+    "url": "/vuejs-gh-pages-gh-actions/img/colorless.8f2a7aaa.svg"
+  },
+  {
+    "revision": "b7d4f57872507c79b1ecd192aad61357",
+    "url": "/vuejs-gh-pages-gh-actions/img/darkness.b7d4f578.svg"
+  },
+  {
+    "revision": "18a1dbc9e3a69ba77e890268451026e4",
+    "url": "/vuejs-gh-pages-gh-actions/img/dragon.18a1dbc9.svg"
+  },
+  {
+    "revision": "603e08d1937f16395d2d304ef6d30ab7",
+    "url": "/vuejs-gh-pages-gh-actions/img/fairy.603e08d1.svg"
+  },
+  {
+    "revision": "8ce4fa81f8f29f8c6871b88f78916637",
+    "url": "/vuejs-gh-pages-gh-actions/img/fighting.8ce4fa81.svg"
+  },
+  {
+    "revision": "33d14b307cd671ab9c1ca76f4788df7a",
+    "url": "/vuejs-gh-pages-gh-actions/img/fire.33d14b30.svg"
+  },
+  {
+    "revision": "8c1e094fa4fa86cf9a4b943d6b6c9253",
+    "url": "/vuejs-gh-pages-gh-actions/img/flying.8c1e094f.svg"
+  },
+  {
+    "revision": "ff3973459922626c46b8935bd3517deb",
+    "url": "/vuejs-gh-pages-gh-actions/img/ghost.ff397345.svg"
+  },
+  {
+    "revision": "d589dd00e175efada7294faf05f52ac6",
+    "url": "/vuejs-gh-pages-gh-actions/img/grass.d589dd00.svg"
+  },
+  {
+    "revision": "289c4927e264d15f5be41d5c53f5c601",
+    "url": "/vuejs-gh-pages-gh-actions/img/ground.289c4927.svg"
+  },
+  {
+    "revision": "f82b7e5d85efb43a4e5001ed490269d4",
+    "url": "/vuejs-gh-pages-gh-actions/img/ice.f82b7e5d.svg"
+  },
+  {
+    "revision": "68b142da814c55f9225db2c16193ba01",
+    "url": "/vuejs-gh-pages-gh-actions/img/lightning.68b142da.svg"
+  },
+  {
+    "revision": "95342da3d89b3644b12d596a41d631c7",
+    "url": "/vuejs-gh-pages-gh-actions/img/metal.95342da3.svg"
+  },
+  {
+    "revision": "183135e96caba5df8c380e0d2acdea6a",
+    "url": "/vuejs-gh-pages-gh-actions/img/poison.183135e9.svg"
+  },
+  {
+    "revision": "844790bc01a65c07d2b136b25826172d",
+    "url": "/vuejs-gh-pages-gh-actions/img/pokemon-fig.844790bc.png"
+  },
+  {
+    "revision": "60982f05e13fbd9b6e6d96ce3456d365",
+    "url": "/vuejs-gh-pages-gh-actions/img/psychic.60982f05.svg"
+  },
+  {
+    "revision": "499a0fa10362807e9a3218082be3d288",
+    "url": "/vuejs-gh-pages-gh-actions/img/rock.499a0fa1.svg"
+  },
+  {
+    "revision": "e29fb8e379f9ed5335a02eb3881d25d6",
+    "url": "/vuejs-gh-pages-gh-actions/img/water.e29fb8e3.svg"
+  },
+  {
+    "revision": "25ec42a08b9fde625c3168d6e2c0454f",
+    "url": "/vuejs-gh-pages-gh-actions/index.html"
+  },
+  {
+    "revision": "b3e8953b235cf6ed7090",
+    "url": "/vuejs-gh-pages-gh-actions/js/app.ee94314f.js"
+  },
+  {
+    "revision": "885746b51c5405f33af3",
+    "url": "/vuejs-gh-pages-gh-actions/js/chunk-7d480729.8b433085.js"
+  },
+  {
+    "revision": "869a596717edc6bbabfe",
+    "url": "/vuejs-gh-pages-gh-actions/js/chunk-vendors.1d4bdf90.js"
+  },
+  {
+    "revision": "d2e98291392701698493e5aaf9a6214b",
+    "url": "/vuejs-gh-pages-gh-actions/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-gh-pages-gh-actions/robots.txt"
+  }
+]);
